0aa96eb24ed5f72e11344f59a4d756c09d02bdbd
Stefanus Tan Jaya <stefanustanjaya230105@gmail.com>
2025-02-16 23:14:52 +0700

    build: Setup Docker

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     java
     jacoco
+    id("org.sonarqube") version "6.0.1.5171"
     id("org.springframework.boot") version "3.4.2"
     id("io.spring.dependency-management") version "1.1.7"
 }
