70f659a4df5d2ef58d4802673e211c5a15d32e41
Stefanus Tan Jaya <stefanustanjaya230105@gmail.com>
2025-02-16 17:41:36 +0700

    feat: Add Product Controller Test

--- src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
@@ -5,7 +5,6 @@ import id.ac.ui.cs.advprog.eshop.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import jakarta.validation.Valid;
@@ -26,10 +25,7 @@ public class ProductController {
     }
 
     @PostMapping("/create")
-    public String createProductPost(@Valid @ModelAttribute Product product, BindingResult result, Model model) {
-        if (result.hasErrors()) {
-            return "createProduct";
-        }
+    public String createProductPost(@Valid @ModelAttribute Product product, Model model) {
         service.create(product);
         return "redirect:list";
     }
@@ -48,16 +44,14 @@ public class ProductController {
         return "editProduct";
     }
 
-    @PostMapping("/edit")
-    public String updateProduct(@Valid @ModelAttribute Product product, BindingResult result, Model model) {
-        if (result.hasErrors()) {
-            return "editProduct";
-        }
-        service.updateProduct(product);
+    @PutMapping("/edit/{productId}")
+    public String updateProduct(@PathVariable String productId, @ModelAttribute Product productUpdate){
+        productUpdate.setProductId(productId);
+        service.updateProduct(productUpdate);
         return "redirect:/product/list";
     }
 
-    @GetMapping("/delete/{productId}")
+    @DeleteMapping("/delete/{productId}")
     public String deleteProduct(@PathVariable("productId") String productId) {
         service.deleteProduct(productId);
         return "redirect:/product/list";
